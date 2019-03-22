34d5aa943be5c60a9c11513081eca4ef7db77cc9
davidjoe0923 <davidjoe0923@gmail.com>
2019-03-22 02:27:45 -0400

    Updates

---
+++ precache-manifest.00a3690283ff5277d8283a0df80ea4f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78d813af433f00fa9a02",
+    "url": "/static/js/main.d672b5da.chunk.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/static/js/2.0c9cd3e4.chunk.js"
+  },
+  {
+    "revision": "78d813af433f00fa9a02",
+    "url": "/static/css/main.1b43d99d.chunk.css"
+  },
+  {
+    "revision": "618229a04422d73d2fd613b6f4f34044",
+    "url": "/index.html"
+  }
+];
